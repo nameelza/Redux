ea2abae0d175f01ca91fee167d49dcf75b615811
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-08 12:46:52 -0800

    reducer

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
+function todos(state = [], action) {
+  if (action === "ADD_TODO") {
+    state.concat([action.todo]);
+  }
+
+  return state;
+}
+
 function createStore() {
-    // The store has four parts:
-    // 1. The state
-    // 2. Get the state
-    // 3. Listen to changes on the state
-    // 4. Update the state
+  // The store has four parts:
+  // 1. The state
+  // 2. Get the state
+  // 3. Listen to changes on the state
+  // 4. Update the state
 
-    let state
-    let listeners = []
+  let state;
+  let listeners = [];
 
-    const getState = () => state
+  const getState = () => state;
 
-    const subscribe = (listener) => {
-        listeners.push(listener)
-        return () => {
-            listeners.filter((l) => l !== listener)
-        }
-    }
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners.filter((l) => l !== listener);
+    };
+  };
 
-    return {
-        getState,
-        subscribe
-    }
+  return {
+    getState,
+    subscribe,
+  };
 }
