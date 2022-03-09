4faa1577531af2fe4126bfe8cf7a9e66d4599ac7
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-09 12:40:40 -0800

    actionCreator

--- index.js
+++ index.js
@@ -31,19 +31,54 @@ function createStore(reducer) {
 }
 
 // App Code
-const ADD_TODO = "ADD_TODO"
-const REMOVE_TODO = "REMOVE_TODO"
-const TOGGLE_TODO = "TOGGLE_TODO"
-const ADD_GOAL = "ADD_GOAL"
-const REMOVE_GOAL = "REMOVE_GOAL"
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  };
+}
 
 function todos(state = [], action) {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case "REMOVE_TODO":
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id);
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return state.map((todo) =>
         todo.id !== action.id
           ? todo
@@ -56,9 +91,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
   switch (action.type) {
-    case "ADD_GOAL":
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case "REMOVE_GOAL":
+    case REMOVE_GOAL:
       return state.filter((goal) => goal.id !== action.id);
     default:
       return state;
@@ -79,11 +114,46 @@ store.subscribe(() => {
   console.log("The new state is: ", store.getState());
 });
 
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+store.dispatch(
+  addTodoAction({
     id: 0,
     name: "Learn Redux",
     complete: false,
-  },
-});
+  })
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 1,
+    name: "Walk the dog",
+    complete: false,
+  })
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 2,
+    name: "Go to the gym",
+    complete: false,
+  })
+);
+
+store.dispatch(removeTodoAction(2));
+
+store.dispatch(toggleTodoAction(1));
+
+store.dispatch(
+  addGoalAction({
+    id: 0,
+    name: "Learn redux",
+  })
+);
+
+store.dispatch(
+  addGoalAction({
+    id: 1,
+    name: "Buy a bike",
+  })
+);
+
+store.dispatch(removeGoalAction(1));
