eabad3e1ae3104089c807bbba3a2de9283297ee0
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-08 16:24:59 -0800

    goals reducer

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ function createStore(reducer) {
 }
 
 // App Code
-function todos(state = [], action) {
+function todos (state = [], action) {
   switch (action.type) {
     case "ADD_TODO":
-      return state.concat([action.todo]);
+      return state.concat([action.todo])
     case "REMOVE_TODO":
-      return state.filter((todo) => todo.id !== action.id);
+      return state.filter((todo) => todo.id !== action.id)
     case "TOGGLE_TODO":
       return state.map((todo) =>
         todo.id !== action.id
@@ -44,7 +44,18 @@ function todos(state = [], action) {
           : Object.assign({}, todo, { complete: !todo.complete })
       );
     default:
-      return state;
+      return state
+  }
+}
+
+function goals (state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state
   }
 }
 
