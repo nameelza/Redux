d2833622b2343e4af077a31dc58f070612fd5815
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-24 16:04:50 -0700

    goals-todos-structure

---
+++ goals-todos/src/actions/todos.js
@@ -0,0 +1,59 @@
+export const ADD_TODO = "ADD_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+
+function addTodo(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+
+function removeTodo(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+
+function toggleTodo(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function handleAddTodo(name, callback) {
+  return (dispatch) => {
+    return API.saveTodo(name)
+      .then((todo) => {
+        dispatch(addTodo(todo));
+        callback();
+      })
+      .catch(() => {
+        alert("An error occurred. Try again");
+      });
+  };
+}
+
+function handleDeleteTodo(item) {
+  return (dispatch) => {
+    dispatch(removeTodo(item.id));
+
+    return API.deleteTodo(item.id).catch(() => {
+      dispatch(addTodo(item));
+      alert("An error occurred. Try again");
+    });
+  };
+}
+
+function handleToggleTodo(id) {
+  return (dispatch) => {
+    dispatch(toggleTodo(id));
+
+    return API.saveTodoToggle(id).catch(() => {
+      dispatch(toggleTodo(id));
+      alert("An error occurred. Try again");
+    });
+  };
+}
